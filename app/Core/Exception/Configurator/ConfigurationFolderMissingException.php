d88f3c36d40cdaa3ee4df25489043b743b858ddf
Rumen Tabakov <rumen.tabakov@gmail.com>
2021-12-28 18:41:10 +0200

    Project moved to php 8.0 as lowest version

--- app/Core/Exception/Configurator/ConfigurationFolderMissingException.php
+++ app/Core/Exception/Configurator/ConfigurationFolderMissingException.php
@@ -4,18 +4,29 @@
  *         rumen.tabakov@gmail.com
  */
 
+declare(strict_types=1);
+
 namespace App\Core\Exception\Configurator;
 
-
+use Exception;
 use Throwable;
 
-class ConfigurationFolderMissingException extends \Exception
+/**
+ * Class ConfigurationFolderMissingException
+ * @package App\Core\Exception\Configurator
+ */
+class ConfigurationFolderMissingException extends Exception
 {
     private const MESSAGE = 'Configurations folder is missing.';
 
-    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    /**
+     * ConfigurationFolderMissingException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $message = self::MESSAGE, int $code = 0, Throwable $previous = null)
     {
-        $message = static::MESSAGE;
         parent::__construct($message, $code, $previous);
     }
 }
